5d4191862353f1a3338422d59a2449ce628bbd71
touatisayed <touati.sayed47@gmail.com>
2025-05-12 23:42:26 +0100

    feat: add Contact entity to represent contact data in the database

--- src/main/java/io/getarrays/contactapi/Application.java
+++ src/main/java/io/getarrays/contactapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactapiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactapiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }

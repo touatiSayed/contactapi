2e141fca4d3af2d82ad45ce3e0457be79ddfe8a6
touatisayed <touati.sayed47@gmail.com>
2025-05-15 23:18:59 +0100

    feat: configure CORS support to enable frontend-backend communication
    - Created CorsConfig.java to allow cross-origin requests from React (localhost:3000) and Angular (localhost:4200)
    - Enabled necessary HTTP methods and headers via CorsConfiguration
    - Added X_REQUESTED_WITH constant in Constant.java for cleaner header references

--- src/main/java/io/getarrays/contactapi/constant/Constant.java
+++ src/main/java/io/getarrays/contactapi/constant/Constant.java
@@ -2,4 +2,6 @@ package io.getarrays.contactapi.constant;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Download/uploads/";
+
+    public static final String X_REQUESTED_WITH = "X-REQUESTED-WITH";
 }
